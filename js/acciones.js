3c4c549385abda01548a1fcd26c79fc2970964ca
Lanix 1 <Lanix 1>
2018-05-23 09:39:05 -0500

    4

--- js/acciones.js
+++ js/acciones.js
@@ -1,22 +1,22 @@
 // JavaScript Document
-$(document).ready(function(e) {//escuchador de eventos
-    document.addEventListener("deviceready",function(){ //deslizar a la isquierda
-		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizo hacia la izquierda",function(){"Aplicacion7", "Ok"}); //aparece mensaje alert
-		}); //cierre swipe isquierda
-		$('#derecha').on("swiperight",function() { //deslizar derecha
-			navigator.notification.confirm("¿Que quieres hacer?",function(op){ //aparece ,mensaje para confirmar
-				switch(op) //variable op para las opcciones 
+$(document).ready(function(e) {
+	document.addEventListener("deviceready",function(){ //escuchador de eventos
+		$('#Izquierda').on("swipeleft",function(){ //deslizar a la izquierda
+			navigator.notification.alert("Deslizó hacia la Izquierda",function(){"aplicacion 7", "Ok"}); //aparece mensaje alert
+		}); //cierre swipe izquierda
+		$('#Derecha').on("swiperight",function(){ //deslizar a la derecha
+			navigator.notification.confirm("¿Que quieres hacer?",function(op){ //aparece mensaje para confirmar
+				switch(op) //variable op para las opciones
 				{
 					case 1:
-					navigator.notification.beep(1); //suena el dispocitivo
-					break;
+					navigator.notification.beep(1); //suena el dispositivo
+					break; 
 					
 					case 2:
-					navigator.notification.vibrate(3000); //vibra el dispocitivo
+					navigator.notification.vibrate(3000); //vibra el dispositivo
 					break;
 				}
 			},"aplicacion7","Beepear,Vibrar,Cancelar");
 			});
-		},false);
-}); 
+    },false);
+});
